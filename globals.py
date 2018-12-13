13d4fa4504fe5dc6afe02dc2c93e61184f964d26
Roei Yehuda <roei9yehuda@gmail.com>
2018-12-13 17:02:11 +0200

    now using only Proxys that do not detected on "https://whoer.net/#extended"

--- globals.py
+++ globals.py
@@ -1,17 +1,21 @@
 import random
 
+# servers took from: https://free-proxy-list.net/
+
 proxys_list = \
     [
-        "97.68.17.66:53281",
-        "136.25.2.43:33773",
-        "65.152.237.149:53281",
-        "159.65.105.99:3128",
-        "176.37.46.44:56144",
-        "178.136.11.7:48816",
-        "190.220.255.115:61376",
-        "46.63.162.171:8080",
+        # "114.34.168.157:46160",  # captcha required
+        # "178.136.11.7:48816",    # suspicious ip
+        # "159.65.105.99:3128",    # proxy detected
+        # "167.99.73.246:3128",    # proxy detected
+        # "209.97.163.194:8080",   # proxy detected
+        # "65.152.237.149:53281",  # proxy detected
+        # "149.56.129.139:8888",   # proxy detected
+        # "45.32.190.16:3128",     # proxy detected
+        "176.37.46.44:56144",      # perfect
+        "97.68.17.66:53281",       # perfect
+        "190.220.255.115:61376"    # perfect
     ]
-
 
 class Proxys(object):
     @staticmethod
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 # project
 from globals import Proxys
 
-on_background = True
+on_background = False
 on_vpn = True
 
 def main():
@@ -18,21 +18,28 @@ def main():
         chrome_options.add_argument("--headless")
 
     if on_vpn:
-        chrome_options.add_argument("--proxy-server={proxy}".format(proxy=Proxys.get_random_proxy()))
+        proxy_addr = Proxys.get_random_proxy()
+        print "proxy: "+ proxy_addr
+        chrome_options.add_argument("--proxy-server={proxy}".format(proxy=proxy_addr))
     else:
         chrome_options.add_argument("--no-proxy-server")
         chrome_options.add_argument("--proxy-bypass-list=*")
         chrome_options.add_argument("--proxy-server='direct://'")
 
+    # adblock:
+    # chrome_options.add_extension(
+    #     r"C:\Users\roei\AppData\Local\Google\Chrome\User Data\Default\Extensions\gighmmpiobklfepjocnamgkkbiglidom")
+
     print "starting..."
     start_time = time()
-    with webdriver.Chrome(options=chrome_options) as driver:
+    with webdriver.Chrome(chrome_options=chrome_options) as driver:
         print "driver is open"
-        driver.get("http://www.whatsmyip.org/")
+        driver.get("https://whoer.net/#extended")
         print driver.title
         assert "IP" in driver.title
-        elem = driver.find_element_by_id("ip")
+        elem = driver.find_element_by_class_name("your-ip")
         print "your ip is now: " + elem.text
+        sleep(10)
     end_time = time()
     print "finished with time: " + str(end_time - start_time)
 
