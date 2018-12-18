2ba3fed437ed36dd79def9f82bfa21623fe71d0c
roei6 <roei9yehuda@gmail.com>
2018-12-19 00:04:33 +0200

    can reach out and click on a ticket

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+
 # system
 from time import sleep
 from time import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 # project
 from globals import Proxys
@@ -14,7 +20,7 @@ def main():
     # using the driver with random proxys
     proxy_address = Proxys.get_random_proxy()
     print "proxy: " + proxy_address
-    start_driver(proxy=proxy_address, display=True)
+    start_driver(proxy=None, display=True)
 
 
 def start_driver(proxy=None, display=False, enable_adblock=False):
@@ -30,13 +36,55 @@ def start_driver(proxy=None, display=False, enable_adblock=False):
 
     start_time = time()
     with webdriver.Chrome(chrome_options=chrome_options) as driver:
-        driver.get("https://globusmax.co.il/movie-1246-spider_man_into_the_spiderverse")
-        refresh_loop(":", driver, 5, 3)
+        driver.get("https://globusmax.co.il/")
+        refresh_loop("-", driver, 5, 3)
         print "done loading the page"
-        elem = driver.find_element_by_xpath('//div[@data-eventid="59503"]')
+
+        # try to close the advertisement
+        try:
+            elem = driver.find_element_by_id('ZA_CANVAS_436229_CLOSE_IMG2_8_IMG')
+            elem.click()
+            print "ad closed"
+        except Exception:
+            print "could not load ad"
+
+        # wait for the ad to disappear
+        sleep(2)
+
+        select_stuff(driver, "cinema", "5")
+        select_stuff(driver, "movie", "1246")
+        select_stuff(driver, "lang", "dub")
+        select_stuff(driver, "date", "20/12/2018")
+        select_stuff(driver, "time", "16:50")
+
+
+        # click on the invite button
+        elem = driver.find_element_by_class_name('send_btn')
         elem.click()
+
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "ddQunatity_0"))
+        )
+
+
+        select_stuff(driver, "ddQunatity_0", "1")
+
+        elem = driver.find_element_by_id('lbSelectSeats')
+        elem.click()
+
+        sleep(3)
+
+        canvas = driver.find_element_by_id("myCanvas")
+        drawing = ActionChains(driver) \
+            .move_to_element(canvas)  \
+            .move_by_offset(-33, 33) \
+            .click()
+        drawing.perform()
+
+
         print "it worked"
-        sleep(15)
+        sleep(120)
     end_time = time()
     return (end_time - start_time)
 
@@ -62,21 +110,8 @@ def get_options(proxy, display, enable_adblock):
     if not display:
         chrome_options.add_argument("--headless")
         # don't load photos
-        prefs = {'profile.default_content_setting_values': {'images': 2,
-                                                            'plugins': 2, 'popups': 2, 'geolocation': 2,
-                                                            'notifications': 2, 'auto_select_certificate': 2,
-                                                            'fullscreen': 2,
-                                                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
-                                                            'media_stream_mic': 2, 'media_stream_camera': 2,
-                                                            'protocol_handlers': 2,
-                                                            'ppapi_broker': 2, 'automatic_downloads': 2,
-                                                            'midi_sysex': 2,
-                                                            'push_messaging': 2, 'ssl_cert_decisions': 2,
-                                                            'metro_switch_to_desktop': 2,
-                                                            'protected_media_identifier': 2, 'app_banner': 2,
-                                                            'site_engagement': 2,
-                                                            'durable_storage': 2}}
-        chrome_options.add_experimental_option("prefs", prefs)
+    # prefs = {'profile.default_content_setting_values': {'images': 2}}
+    # chrome_options.add_experimental_option("prefs", prefs)
 
     # setting a proxy to the given argument, if not then don't use proxy
     if proxy is not None:
@@ -91,6 +126,18 @@ def get_options(proxy, display, enable_adblock):
             r"load-extension=C:\Users\roei\PycharmProjects\cinematic_bot\extentions\adblock")
 
     return chrome_options
+
+
+def select_stuff(driver, attribute, value):
+    select_script_js = """
+            const change_ev = new Event("change");
+            const {attribute}_name = document.querySelector('#{attribute}');
+            {attribute}_name.value = "{value}";
+            {attribute}_name.dispatchEvent(change_ev);
+            """.format(attribute=attribute, value=value)
+    driver.execute_script(select_script_js)
+
+    sleep(2)
 
 
 if __name__ == '__main__':
