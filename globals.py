37ae06126c5d48aeb2c9eb35994bf02ffd68df5e
roei6 <roei9yehuda@gmail.com>
2018-12-19 00:04:33 +0200

    can reach out and click on a ticket

--- globals.py
+++ globals.py
@@ -1,14 +1,11 @@
 import random
 
-
-
 class Proxys(object):
     # servers took from: https://free-proxy-list.net/
     proxys_list = \
         [
             "176.37.46.44:56144",
             "97.68.17.66:53281",
-            "202.43.113.18:34856",  # no internet?
             "195.189.89.122:32193",
             "190.210.245.200:60539",
             "185.31.163.21:35786",
