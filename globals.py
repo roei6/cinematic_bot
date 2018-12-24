317a2c3d8fd2344ae41d16aa068712975f6795f7
roei6 <roei9yehuda@gmail.com>
2018-12-24 14:08:26 +0200

    movieinviter built without breaking the code

--- globals.py
+++ globals.py
@@ -1,5 +1,31 @@
 import random
 
+
+class Invitation(object):
+    """
+    contains all the invitation information for ordering the movie,
+    """
+    def __init__(self, site, cinema_id, movie_id, date, time, seats_places, movie_type="normal", lang="sub"):
+        """
+        :type site: str
+        :type cinema_id: int
+        :type movie_id: int
+        :type date: str
+        :type time: str
+        :type seats_places: dict
+        :type movie_type: str
+        :type lang: str
+        """
+        self.site = site
+        self.cinema_id = cinema_id
+        self.movie_id = movie_id
+        self.date = date
+        self.time = time
+        self.lang = lang
+        self.movie_type = movie_type
+        self.seats_places = seats_places
+
+
 class Proxys(object):
     # servers took from: https://free-proxy-list.net/
     proxys_list = \
@@ -14,7 +40,7 @@ class Proxys(object):
             "1.20.101.95:49062",
             # "190.220.255.115:61376"  # ?
         ]
+
     @staticmethod
     def get_random_proxy():
         return random.choice(Proxys.proxys_list)
-
