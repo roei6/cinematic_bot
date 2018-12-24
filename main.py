d9deeef846ccd7991637f323c4dcd3a89efa36d9
roei6 <roei9yehuda@gmail.com>
2018-12-24 16:50:45 +0200

    implemented with seats_selector

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ from movie_inviter import MovieInviter
 def main():
     # argparse that will get the details or interactively
     invitation = Invitation(site="https://globusmax.co.il/",
-                            cinema_id=2, movie_id=1117, date="26/12/2018", time="13:50",
-                            seats_places={8: [7]})
+                            cinema_id=2, movie_id=1117, date="26/12/2018", time="13:20", lang="dub",
+                            seats_places=
+                            {
+                                7: [7, 6, 5],
+                                9: [1, 2, 3]
+                            })
 
     # using the driver with random proxys
     proxy_address = Proxys.get_random_proxy()
     print "proxy: " + proxy_address
     movie_inviter = MovieInviter(proxy=None, display=True)
     movie_inviter.invite(invitation)
-
-
-
 
 
 if __name__ == '__main__':
