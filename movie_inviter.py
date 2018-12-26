578238dbebda5a567f9d139ec06ea13c6a3f1ff1
roei6 <roei9yehuda@gmail.com>
2018-12-26 16:38:51 +0200

    proxys now work way better

--- movie_inviter.py
+++ movie_inviter.py
@@ -13,6 +13,7 @@ from netmotorist import NetMotorist
 class MovieInviter(object):
     def __init__(self, proxy=None, display=False, enable_adblock=False):
         self.driver = NetMotorist(chrome_options=self.__get_options(proxy, display, enable_adblock))
+        self.driver.implicitly_wait(30)
 
     def __del__(self):
         self.driver.close()
@@ -34,6 +35,8 @@ class MovieInviter(object):
         except Exception:
             print "could not load ad"
 
+        print "selecting the movie stuff"
+
         self.driver.select_stuff("cinema", invitation.cinema_id)
         self.driver.select_stuff("movie", invitation.movie_id)
         self.driver.select_stuff("lang", invitation.lang)
@@ -42,9 +45,11 @@ class MovieInviter(object):
 
         sleep(5)
 
-        # click on the invite button
+        # click on the invite button # sould be done in js
         elem = self.driver.find_element_by_class_name('send_btn')
         elem.click()
+
+        print "selecting tickets"
 
         seats_selector = SeatsSelector(self.driver)
         seats_selector.save_seats(invitation.seats_places)
